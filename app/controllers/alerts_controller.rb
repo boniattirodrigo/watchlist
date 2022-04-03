303858b927192ab27ead310baf0a70a551937947
Rodrigo Boniatti <boniatti.rodrigo@gmail.com>
2022-04-03 17:33:42 -0300

    Add alerts

--- app/controllers/alerts_controller.rb
+++ app/controllers/alerts_controller.rb
@@ -2,6 +2,10 @@ class AlertsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @alerts = Alert.all
+    assets_id = Wallet.find_by(user_id: current_user.id).items.pluck(:asset_id)
+
+    @alerts = Alert.joins(quote: :asset)
+                   .where(quotes: { asset_id: assets_id })
+                   .order(created_at: :desc)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  resources :alerts, only: %i[index]
   devise_for :users
 
   root to: 'wallets#index'
 
   post '/wallet/items', to: 'wallet_items#create'
   delete '/wallet/items', to: 'wallet_items#destroy'
+  get '/alerts', to: 'alerts#index'
 end
