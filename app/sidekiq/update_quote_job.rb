95f665a87edd116b06c3ada713721d791cc3a3f4
mateuscelio <mateuscelio10@gmail.com>
2022-04-07 18:05:07 -0300

    Limit retries and creates alert

--- app/sidekiq/update_quote_job.rb
+++ app/sidekiq/update_quote_job.rb
@@ -1,5 +1,10 @@
 class UpdateQuoteJob
   include Sidekiq::Job
+  sidekiq_options retry: 5
+
+  sidekiq_retries_exhausted do |msg, _ex|
+    Alert.create(message: "Failed to update quote price for '#{msg['args'][0]}': #{msg['error_message']}")
+  end
 
   def perform(asset_symbol)
     Quotes::AssetQuoteUpdater.call(asset_symbol)
